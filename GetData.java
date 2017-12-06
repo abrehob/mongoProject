a900d23402c87d94afdd73d98917f84c2eb0fbf4
jmpat <jmpat@umich.edu>
2017-12-05 22:06:47 -0500

    jmpat's GetData.java

--- GetData.java
+++ GetData.java
@@ -4,10 +4,9 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.TreeSet;
-import java.util.Vector;
-
-
+//import java.util.TreeSet;
+//import java.util.Vector;
+import java.util.*;
 
 //json.simple 1.1
 // import org.json.simple.JSONObject;
@@ -17,8 +16,8 @@ import java.util.Vector;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
-public class GetData{
-	
+public class GetData
+{
     static String prefix = "jiaqni.";
 	
     // You must use the following variable as the JDBC connection
@@ -47,75 +46,152 @@ public class GetData{
 
 	
     // DO NOT modify this constructor
-    public GetData(String u, Connection c) {
-	super();
-	String dataType = u;
-	oracleConnection = c;
-	// You will use the following tables in your Java code
-	cityTableName = prefix+dataType+"_CITIES";
-	userTableName = prefix+dataType+"_USERS";
-	friendsTableName = prefix+dataType+"_FRIENDS";
-	currentCityTableName = prefix+dataType+"_USER_CURRENT_CITY";
-	hometownCityTableName = prefix+dataType+"_USER_HOMETOWN_CITY";
-	programTableName = prefix+dataType+"_PROGRAMS";
-	educationTableName = prefix+dataType+"_EDUCATION";
-	eventTableName = prefix+dataType+"_USER_EVENTS";
-	albumTableName = prefix+dataType+"_ALBUMS";
-	photoTableName = prefix+dataType+"_PHOTOS";
-	tagTableName = prefix+dataType+"_TAGS";
+    public GetData(String u, Connection c)
+    {
+	    super();
+	    String dataType = u;
+	    oracleConnection = c;
+	    // You will use the following tables in your Java code
+	    cityTableName = prefix+dataType+"_CITIES";
+	    userTableName = prefix+dataType+"_USERS";
+	    friendsTableName = prefix+dataType+"_FRIENDS";
+	    currentCityTableName = prefix+dataType+"_USER_CURRENT_CITY";
+	    hometownCityTableName = prefix+dataType+"_USER_HOMETOWN_CITY";
+	    programTableName = prefix+dataType+"_PROGRAMS";
+	    educationTableName = prefix+dataType+"_EDUCATION";
+	    eventTableName = prefix+dataType+"_USER_EVENTS";
+	    albumTableName = prefix+dataType+"_ALBUMS";
+	    photoTableName = prefix+dataType+"_PHOTOS";
+	    tagTableName = prefix+dataType+"_TAGS";
     }
 	
-	
-	
-	
     //implement this function
-
     @SuppressWarnings("unchecked")
-    public JSONArray toJSON() throws SQLException{ 
+    public JSONArray toJSON() throws SQLException
+    {
+	    // Your implementation goes here....		
+		JSONArray users_info = new JSONArray();
 		
-	// Your implementation goes here....		
+		try (Statement stmtU = oracleConnection.createStatement
+               (ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+             Statement stmtF = oracleConnection.createStatement
+               (ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);)
+        {
+            ResultSet rstU = stmtU.executeQuery("SELECT user_id, first_name, last_name, "+
+              "gender, year_of_birth, month_of_birth, day_of_birth FROM "+userTableName);
+              
+            HashMap<Long,Integer> idIndexMap = new HashMap<Long,Integer>();
+            int i = 0;
 
+            // Loop through result set
+            Long userID;
+            String firstName;
+            String lastName;
+            String gender;
+            int year;
+            int month;
+            int day;
+            String city = "";
+            String state = "";
+            String country = "";
+            
+            while (rstU.next())
+            {
+                userID = rstU.getLong(1);
+                firstName = rstU.getString(2);
+                lastName = rstU.getString(3);
+                gender = rstU.getString(4);
+                year = rstU.getInt(5);
+                month = rstU.getInt(6);
+                day = rstU.getInt(7);
+                
+                JSONObject u = new JSONObject();
+                u.put("user_id", userID);
+	            u.put("first_name", firstName);
+	            u.put("last_name", lastName);
+	            u.put("gender", gender);
+	            u.put("YOB", year);
+	            u.put("MOB", month);
+	            u.put("DOB", day);
+	            u.put("friends", new JSONArray());
+	            users_info.put(u);
+                
+                idIndexMap.put(userID, i);
+                i += 1;
+
+                Statement stmtC = oracleConnection.createStatement
+                  (ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                ResultSet rstC = stmtC.executeQuery
+                 ("SELECT C.city_name, C.state_name, C.country_name "+
+                  "FROM "+cityTableName+" C, "+hometownCityTableName+" UHC "+
+                  "WHERE UHC.user_id = "+userID+" AND UHC.hometown_city_id = C.city_id");
+
+                // Get the hometown of the current user. Should only
+                // go through one iteration of the while loop.
+                while (rstC.next())
+                {
+                    city = rstC.getString(1);
+                    state = rstC.getString(2);
+                    country = rstC.getString(3);
+                }
+                
+                JSONObject h = new JSONObject();
+                h.put("city", city);
+                h.put("state", state);
+                h.put("country", country);
+                u.put("hometown", h);
+                
+                rstC.close();
+                stmtC.close();
+            }
+            rstU.close();
+            stmtU.close();
+            
+            Long u1_userID;
+            Long u2_userID;
+            ResultSet rstF = stmtF.executeQuery("SELECT user1_id, user2_id FROM "+friendsTableName);
+            while (rstF.next())
+            {
+                u1_userID = rstF.getLong(1);
+                u2_userID = rstF.getLong(2);
+                if (u1_userID < u2_userID)
+                {
+                    users_info.getJSONObject(idIndexMap.get(u1_userID))
+                      .getJSONArray("friends").put(u2_userID);
+                }
+                else
+                {
+                    users_info.getJSONObject(idIndexMap.get(u2_userID))
+                      .getJSONArray("friends").put(u1_userID);
+                }
+            }
+            
+            rstF.close();
+            stmtF.close();
+        }
+        catch (SQLException err)
+        {
+            System.err.println(err.getMessage());
+        }
 		
-		
-	// This is an example usage of JSONArray and JSONObject
-	// The array contains a list of objects
-	// All user information should be stored in the JSONArray object: users_info
-	// You will need to DELETE this stuff. This is just an example.
-
-	// A JSONObject is an unordered collection of name/value pairs. Add a few name/value pairs.
-	JSONObject test = new JSONObject();	// declare a new JSONObject
-	// A JSONArray consists of multiple JSONObjects. 
-	JSONArray users_info = new JSONArray();
-
-	test.put("user_id", "testid");		// populate the JSONObject
-	test.put("first_name", "testname");
-
-	JSONObject test2 = new JSONObject();
-	test2.put("user_id", "test2id");
-	test2.put("first_name", "test2name");
-
-	// users_info.add(test);			// add the JSONObject to JSONArray	
-	// users_info.add(test2);			// add the JSONObject to JSONArray	
-
-	// Use put method if using the alternate JSON modules.
-	users_info.put(test);		// add the JSONObject to JSONArray     
-	users_info.put(test2);		// add the JSONObject to JSONArray	
-	return users_info;
+	    return users_info;
     }
 
     // This outputs to a file "output.json"
-    public void writeJSON(JSONArray users_info) {
-	// DO NOT MODIFY this function
-	try {
-	    FileWriter file = new FileWriter(System.getProperty("user.dir")+"/output.json");
-	    file.write(users_info.toString());
-	    file.flush();
-	    file.close();
-
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
-		
+    public void writeJSON(JSONArray users_info)
+    {
+	    // DO NOT MODIFY this function
+	    try
+	    {
+	        FileWriter file = new FileWriter(System.getProperty("user.dir")+"/output.json");
+	        file.write(users_info.toString());
+	        file.flush();
+	        file.close();
+	    }
+	    catch (IOException e)
+	    {
+	        e.printStackTrace();
+	    }
     }
 }
 
